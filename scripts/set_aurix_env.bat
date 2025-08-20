aa9bdb1b641a207aba1f820546988e874c2d7653
sweta <swetabardemirafra.com>
2025-08-20 12:24:46 +0530

    Updated batchfile

--- scripts/set_aurix_env.bat
+++ scripts/set_aurix_env.bat
@@ -1,4 +1,4 @@
-set PATH=C:\Infineon\AURIX-Studio-1.10.10\tools\Compilers\tricore-gcc11\bin;C:\subbu_home\Unix_utilities\make-4.4.1-with-guile-w32-bin\bin;%PATH%
+set PATH=C:\Infineon\AURIX-Studio-1.10.10\tools\Compilers\tricore-gcc11\bin;C:\project\Blinky_LED_1_KIT_TC397_TFT\scripts\make-4.4.1-with-guile-w32-bin\bin;%PATH%
 cd "C:\project\Blinky_LED_1_KIT_TC397_TFT"
 
 
